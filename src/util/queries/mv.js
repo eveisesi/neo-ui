304b6cacef345c42f5508122d2522523b568afa9
David Douglas <david@onetwentyseven.dev>
2020-06-13 00:47:29 +0000

    Actually send the id with the query, dumbass

--- src/util/queries/mv.js
+++ src/util/queries/mv.js
@@ -4,13 +4,14 @@ const MOST_VALUABLE = gql`
     query(
         $category: Category = all
         $type: Entity = all
+        $id: Int!
         $age: Int = 7
         $limit: Int = 6
     ) {
         mv: mvByEntityID(
             category: $category
             entity: $type
-            id: 0
+            id: $id
             age: $age
             limit: $limit
         ) {
